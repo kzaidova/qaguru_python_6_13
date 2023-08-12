57b842cb7ff974b71f3350061970335c702d47b8
kzaidova <katerina00.00@bk.ru>
2023-08-12 17:07:52 +0400

    - refactor path to file

--- demoqa_tests/resource.py
+++ demoqa_tests/resource.py
@@ -1,6 +1,9 @@
 from pathlib import Path
 
+import tests
+
+
 def path(file_name):
     return str(
-        Path(qaguru_python_6_13.__file__).parent.joinpath(f'resources/{file_name}').absolute()
+        Path(tests.__file__).parent.joinpath(f'resources/{file_name}').absolute()
     )
